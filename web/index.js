4a58bf6c48086953de6b72af70019fbfc9df9369
unknown <bdavis@redspin.net>
2015-11-14 22:21:01 -0500

    cors resolved

--- web/index.js
+++ web/index.js
@@ -1,13 +1,19 @@
 // Our custom code goes here.
-var baseurl = "api.tunes.redspin.net"
+// Developing Locally?  Swap the comments here and start the python server.
+var baseurl = "http://api.tunes.redspin.net"
+//var baseurl = "http://localhost:5000"
 
 function test(){
     return("true");
 }
 
-function testAjax(){
-    $.ajax({
-        url: "mysql/counts",
-        context: document.body
-    });
+function testAjax () { 
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (xhttp.readyState == 4 && xhttp.status == 200) {
+     console.log("workd" + xhttp.responseText);
+    }
+  };
+  xhttp.open("GET", baseurl + "/mysql/counts", true);
+  xhttp.send();
 }
