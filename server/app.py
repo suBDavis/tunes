f66bebbff89bf3ff5de27dce49ca1b72571f1a76
unknown <bdavis@redspin.net>
2015-11-15 11:50:25 -0500

    template engine

--- server/app.py
+++ server/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 from utils import SQL, Config
 from flask.ext.cors import CORS
 
@@ -8,22 +8,26 @@ CORS(app)
 sql = SQL(config)
 
 @app.route("/")
+def index():
+    return render_template('index.html', id=config.getVersion())
+
+@app.route("/api")
 def hello():
     return "Spotitunes API Version " + config.getVersion()
 
-@app.route("/mysql")
+@app.route("/api/mysql")
 def mysql():
     return jsonify(sql.getConnectionData())
 
-@app.route("/playlist/<plid>/tracks")
+@app.route("/api/playlist/<plid>/tracks")
 def tracksByPlaylist(plid):
     return jsonify(sql.tracksByPlaylist(plid))
 
-@app.route("/mysql/counts")
+@app.route("/api/mysql/counts")
 def mysqlCounts():
     return jsonify(sql.allCounts())
 
-@app.route("/artists/<name>")
+@app.route("/api/artists/<name>")
 def topArtists(name):
     return jsonify(sql.topArtists(name))
 
