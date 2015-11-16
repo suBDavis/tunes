1686bff560eb3f00bc5fc9bc81eda7e1081a4733
unknown <bdavis@redspin.net>
2015-11-16 01:33:27 -0500

    Soundcloud

--- server/static/index.js
+++ server/static/index.js
@@ -4,20 +4,44 @@ var baseurl = "http://tunes.redspin.net"
 //var baseurl = "http://localhost:5000"
 
 function updateSearch(){
-    var terms = $("#search").val();
-    var callback = function(values){
-        //this is the callback for the AJAX - let's update search
-        values = JSON.parse(values);
-        //console.log(values);
-        var collection = $("#search-results");
+  //empty the list
+  var collection = $("#search-results");
+  //get the search terms from the page
+  var terms = $("#search").val();
+  //create callback function we can pass to our page updaters
+  var callbackdb = function(values){
+    var callbacksc = function(res){
+      //this is the callback for the AJAX - let's update search
+      //console.log(res);
+      if(terms==$("#search").val()){
+        //the terms didnt change.
         collection.empty();
+        //var collection = $("#search-results");
+        for(i=0;i< res.length;i++){
+            //console.log(values.search_result[i])
+            var newli = $("<li class='collection-item sc'>"+ res[i].title + " " + res[i].permalink + "</li>");
+            collection.append(newli);
+            if (i >=5){break;}
+        }
+        values = JSON.parse(values);
         for(i=0;i< values.search_result.length;i++){
             //console.log(values.search_result[i])
-            var newli = $("<li class='collection-item'>"+ values.search_result[i].result + " " + values.search_result[i].result2 + "</li>");
-            collection.append(newli);
+            var newli = $("<li class='collection-item db'>"+ values.search_result[i].result + " " + values.search_result[i].result2 + "</li>");
+            collection.prepend(newli);
+            if (i >=5){break;}
         }
+      }
     }
-    ajax("/api/search/" + terms, callback);
+    if (terms==$("#search").val()){
+      SC.get('/tracks', {
+        q: terms, license: 'cc-by-sa'
+      }).then(function(tracks){
+        callbacksc(tracks);
+        console.log(tracks);
+      });
+    }
+  }
+  ajax("/api/search/" + terms, callbackdb);
 }
 
 function ajax(url, callback) { 
@@ -35,6 +59,10 @@ function ajax(url, callback) {
 function initialize(){
     //register listener for search box.
     $("#search").on('input', function(){ updateSearch(); });
+    //Let's create a soundcloud API connection
+    SC.initialize({
+      client_id : "463bb2a042fa56ed7e95c35b7bf4d615"
+    });
 }
 
 $(document).ready(function() {
