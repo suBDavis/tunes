72b393ffc17c9b0968226c113b883ecb7462f0a2
unknown <bdavis@redspin.net>
2015-11-17 00:14:41 -0500

    some things

--- server/static/index.js
+++ server/static/index.js
@@ -3,11 +3,33 @@
 var baseurl = "http://tunes.redspin.net"
 //var baseurl = "http://localhost:5000"
 
+function addSearchResult(origin, string, cssclass){
+  // var newres = $("<a class='result'></a>");
+  // newres = "<a class='result'></a>";
+  // var newcontent = $("<div class='content'></div>");
+  // newcontent = "<div class='content'></div>";
+  // var newtitle = $("<div class='title'>"+string+"</div>")
+  // newtitle = "<div class='title'>"+string+"</div>";
+  var newli = $("<li class='collection-item "+cssclass+"'>"+ string + "</li>");
+  $("#search-results").append(newli);
+  //newres.append(newcontent);
+  //newcontent.append(newtitle);
+  
+  console.log(string);
+}
+
 function updateSearch(){
   //empty the list
   var collection = $("#search-results");
   //get the search terms from the page
   var terms = $("#search").val();
+  if(terms == ""){
+    //hide search bar, it's blank.
+    collection.hide();
+  } else if (terms.length ==1){
+    //added a letter, show it again
+    collection.show();
+  }
   //create callback function we can pass to our page updaters
   var callbackdb = function(values){
     var callbacksc = function(res){
@@ -17,17 +39,19 @@ function updateSearch(){
         //the terms didnt change.
         collection.empty();
         //var collection = $("#search-results");
-        for(i=0;i< res.length;i++){
-            //console.log(values.search_result[i])
-            var newli = $("<li class='collection-item sc'>"+ res[i].title + " " + res[i].permalink + "</li>");
-            collection.append(newli);
-            if (i >=5){break;}
-        }
         values = JSON.parse(values);
         for(i=0;i< values.search_result.length;i++){
+            console.log(values.search_result[i])
+            //var newli = $("<li class='collection-item db'>"+ values.search_result[i].result + " " + values.search_result[i].result2 + "</li>");
+            addSearchResult(collection,values.search_result[i].result + " " + values.search_result[i].result2, "db");
+            //collection.prepend(newli);
+            if (i >=5){break;}
+        }
+        for(i=0;i< res.length;i++){
             //console.log(values.search_result[i])
-            var newli = $("<li class='collection-item db'>"+ values.search_result[i].result + " " + values.search_result[i].result2 + "</li>");
-            collection.prepend(newli);
+            //var newli = $("<li class='collection-item sc'>"+ res[i].title + " " + res[i].permalink + "</li>");
+            //collection.append(newli);
+            addSearchResult(collection, res[i].title + " " + res[i].permalink, "sc");
             if (i >=5){break;}
         }
       }
